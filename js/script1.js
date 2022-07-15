e7f23277b8c071a7a74a752687a453aa299f8c28
Tiago H <tiagohirute@gmail.com>
2022-07-14 22:36:38 -0300

    first commmit

--- js/script1.js
+++ js/script1.js
@@ -2,19 +2,29 @@
 class Produto {//classe começando com letra maiuscula
 
     constructor() {//inicializador de atributos
-        this.id = 0 //dentro dessa classe, o id vai inicializar em 0
-        this.nomeProduto = ""
-        this.valor = 0
+        this.id = 1 //dentro dessa classe, o id vai inicializar em 1
     }
-
-    
 
     //método
-    adicionar(){
-        alert("MétodoAdicionar")
+    salvar(){//ao clicar no botão pra salvar já vai executar o método lerDados
+        let produto = this.lerDados()// produto irá receber os dados do método lerDados(id, nome do produto e o preço)
     }
 
-    excluir(){
+    lerDados(){
+        let produto = {//objeto
+            // Os atributos serão adicionados dinamicamente.
+            //nomeProduto;
+            //preco;
+        }
+        
+        produto.id = this.id // adiciona o atributo id que está nessa(this) classe(class Produto) no objeto produto 
+        produto.nomeProduto = document.getElementById("produto").value//adicionando dinamicamente o atributo nomeProduto no objeto produto
+        produto.preco = document.getElementById("preco").value//pega todo o valor do input com id produto
+
+        return produto
+    }
+
+    cancelar(){
         alert("MétodoExcluir")
     }
 }
---
+++ js/teste.js
@@ -0,0 +1,22 @@
+class Nome{ //ESSA POARH É SÓ UM MOLDE COM OS VALORES   
+    constructor(valor){
+        this._nome = valor
+        
+    }
+
+    //_nome; (Tiago)
+    //dinamico;
+    //dinamco2;
+    //nulo
+}
+
+let algo = new Nome("Tiago") // ISSO SIM É UM OBJ
+
+let algo2 = new Nome()
+algo2.dinamico = 000
+
+algo.dinamico = 123
+algo.dinamico2 = "StringQualquer"
+algo.nulo = null
+//console.log(algo._nome)
+console.log(algo.dinamico)
